10a27454ca02905f33d1fa7f7e918211d27ab882
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-07-16 15:16:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,6 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, 
 
 from sample_config import Config
 from helpers.database.access_db import db
-from helpers.check_gap import CheckTimeGap
 from plugins.broadcast import broadcast_handler
 from helpers.database.add_user import AddUserToDatabase
 
@@ -38,47 +37,10 @@ ShreeBot = Client(
         plugins=plugins
     )
 
-@ShreeBot.on_message(filters.private & filters.photo & ~filters.edited)
-async def photo_handler(bot: Client, event: Message):
-    await AddUserToDatabase(bot, event)
-    FSub = await ForceSub(bot, event)
-    if FSub == 400:
-        return
-    editable = await event.reply_text("Please Wait ...")
-    await db.set_thumbnail(event.from_user.id, thumbnail=event.photo.file_id)
-    await editable.edit("Permanent Custom Thumbnail Saved Successfully!")
-
-
-@ShreeBot.on_message(filters.private & filters.command(["delete_caption", "del_caption", "remove_caption", "rm_caption"]) & ~filters.edited)
-async def delete_caption(bot: Client, event: Message):
-    await AddUserToDatabase(bot, event)
-    FSub = await ForceSub(bot, event)
-    if FSub == 400:
-        return
-    await db.set_caption(event.from_user.id, caption=None)
-    await event.reply_text("Custom Caption Removed Successfully!")
-
 
 @ShreeBot.on_message(filters.private & filters.command("broadcast") & filters.user(Config.BOT_OWNER) & filters.reply)
 async def _broadcast(_, event: Message):
     await broadcast_handler(event)
-
-
-@ShreeBot.on_message(filters.private & filters.command("status") & filters.user(Config.BOT_OWNER))
-async def show_status_count(_, event: Message):
-    total, used, free = shutil.disk_usage(".")
-    total = humanbytes(total)
-    used = humanbytes(used)
-    free = humanbytes(free)
-    cpu_usage = psutil.cpu_percent()
-    ram_usage = psutil.virtual_memory().percent
-    disk_usage = psutil.disk_usage('/').percent
-    total_users = await db.total_users_count()
-    await event.reply_text(
-        text=f"**Total Disk Space:** {total} \n**Used Space:** {used}({disk_usage}%) \n**Free Space:** {free} \n**CPU Usage:** {cpu_usage}% \n**RAM Usage:** {ram_usage}%\n\n**Total Users in DB:** `{total_users}`",
-        parse_mode="Markdown",
-        quote=True
-    )
 
     Config.AUTH_USERS
     ShreeBot.run()
