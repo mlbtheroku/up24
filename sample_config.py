86f88cd43d38ab0343255c3880271f0604a7d096
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-07-16 17:49:05 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -63,23 +63,12 @@ class Config(object):
     
     
     API_ID = int(os.environ.get("API_ID", 12345))
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     SESSION_NAME = os.environ.get("SESSION_NAME", "1shree")
     SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 5))
     BOT_OWNER = os.environ.get("BOT_OWNER", 1445283714)
-    CAPTION = "Rename Bot by @{}\n\nMade by @AbirHasan2005"
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
+    CAPTION = "Uploaded by @All_Movie_Rockers"
     MONGODB_URI = os.environ.get("MONGODB_URI", "")
     DOWNLOAD_PATH = os.environ.get("DOWNLOAD_PATH", "./downloads")
-    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-    ONE_PROCESS_ONLY = bool(os.environ.get("ONE_PROCESS_ONLY", False))
-    START_TEXT = """
-Hi"""
-    PROGRESS = """
-Percentage : {0}%
-Done: {1}
-Total: {2}
-Speed: {3}/s
-ETA: {4}
-    """
+    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+    START_TEXT = ""
+    PROGRESS = ""
